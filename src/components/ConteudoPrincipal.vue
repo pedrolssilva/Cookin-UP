6baa27dcf85106be3fc5bd8c8bcf50bb466b40fa
Pedro Luis <pedrolssilva@hotmail.com>
2024-08-19 18:53:43 -0300

    Push project

---
+++ src/components/ConteudoPrincipal.vue
@@ -0,0 +1,88 @@
+<script lang="ts">
+import SelecionarIngredientes from './SelecionarIngredientes.vue'
+import SuaLista from './SuaLista.vue'
+import Tag from './Tag.vue'
+import BotaoPrincipal from './BotaoPrincipal.vue'
+
+export default {
+  data() {
+    return {
+      ingredientes: [] as Array<string>,
+    }
+  },
+  components: { SelecionarIngredientes, Tag, SuaLista, BotaoPrincipal },
+  methods: {
+    adicionarIngrediente(ingrediente: string) {
+      this.ingredientes.push(ingrediente)
+    },
+    removerIngrediente(ingrediente: string) {
+      const index = this.ingredientes.indexOf(ingrediente)
+      this.ingredientes.splice(index, 1)
+    },
+  },
+}
+</script>
+
+<template>
+  <main class="conteudo-principal">
+    <SuaLista :ingredientes="ingredientes" />
+    <SelecionarIngredientes
+      @adicionar-ingrediente="adicionarIngrediente($event)"
+      @remover-ingrediente="removerIngrediente($event)"
+    />
+    <BotaoPrincipal texto="Buscar receitas!" />
+  </main>
+</template>
+
+<style scoped>
+.conteudo-principal {
+  padding: 6.5rem 7.5rem;
+  border-radius: 3.75rem 3.75rem 0rem 0rem;
+  background: var(--creme, #fffaf3);
+  color: var(--cinza, #444);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
+}
+
+.sua-lista-texto {
+  color: var(--coral, #f0633c);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+.ingredientes-sua-lista {
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
+}
+
+.lista-vazia {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+
+  color: var(--coral, #f0633c);
+  text-align: center;
+}
+
+@media only screen and (max-width: 1300px) {
+  .conteudo-principal {
+    padding: 5rem 3.75rem;
+    gap: 3.5rem;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .conteudo-principal {
+    padding: 4rem 1.5rem;
+    gap: 4rem;
+  }
+}
+</style>
---
+++ src/components/SuaLista.vue
@@ -0,0 +1,82 @@
+<script lang="ts">
+import { type PropType } from 'vue'
+import Tag from './Tag.vue'
+export default {
+  props: {
+    ingredientes: { type: Array as PropType<string[]>, required: true },
+  },
+  components: { Tag },
+}
+</script>
+
+<template>
+  <section>
+    <span class="subtitulo-lg sua-lista-texto"> Sua lista: </span>
+    <ul v-if="ingredientes.length" class="ingredientes-sua-lista">
+      <li v-for="(ingrediente, index) in ingredientes" :key="index">
+        <Tag :texto="ingrediente" ativa />
+      </li>
+    </ul>
+
+    <p v-else class="paragrafo lista-vazia">
+      <img
+        src="../assets/images/icones/lista-vazia.svg"
+        alt="Ícone de pesquisa"
+      />
+      Sua lista está vazia, selecione ingredientes para iniciar.
+    </p>
+  </section>
+</template>
+
+<style scoped>
+.conteudo-principal {
+  padding: 6.5rem 7.5rem;
+  border-radius: 3.75rem 3.75rem 0rem 0rem;
+  background: var(--creme, #fffaf3);
+  color: var(--cinza, #444);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 5rem;
+}
+
+.sua-lista-texto {
+  color: var(--coral, #f0633c);
+  display: block;
+  text-align: center;
+  margin-bottom: 1.5rem;
+}
+
+.ingredientes-sua-lista {
+  display: flex;
+  justify-content: center;
+  gap: 1rem 1.5rem;
+  flex-wrap: wrap;
+}
+
+.lista-vazia {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 0.25rem;
+
+  color: var(--coral, #f0633c);
+  text-align: center;
+}
+
+@media only screen and (max-width: 1300px) {
+  .conteudo-principal {
+    padding: 5rem 3.75rem;
+    gap: 3.5rem;
+  }
+}
+
+@media only screen and (max-width: 767px) {
+  .conteudo-principal {
+    padding: 4rem 1.5rem;
+    gap: 4rem;
+  }
+}
+</style>
